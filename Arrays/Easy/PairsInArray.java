76c418aecd00695cc1339db2868af7d41ab89d73
vishakhamishra <vidhumishra2919@gmai.com>
2026-04-23 09:56:52 +0530

    Added Problem Pairs in Array

---
+++ Arrays/Easy/PairsInArray.java
@@ -0,0 +1,15 @@
+public class PairsInArray {
+    public static void pairsInarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("("+ curr + " , "+ arr[j]+ ")");
+            }
+            System.out.println(" ");
+        }
+        }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        pairsInarray(arr);
+    }
+}
